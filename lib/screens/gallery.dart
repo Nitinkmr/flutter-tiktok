643652518235e5f41456bdadef937d6e574a311c
nitinkmr <knitin917@gmail.com>
2021-06-02 15:06:18 +0530

    store to firebase on tick button press

--- lib/screens/gallery.dart
+++ lib/screens/gallery.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,8 @@ import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:video_compress/video_compress.dart';
 import 'package:video_player/video_player.dart';
+
+import 'feed_screen.dart';
 
 class Gallery extends StatefulWidget {
   Gallery({Key key}) : super(key: key);
@@ -19,8 +22,18 @@ class _GalleryScreenState extends State<Gallery> {
   File _galleryVideo;
   final _firebaseStorage = FirebaseStorage.instance;
 
+  static const IconData checkIcon = IconData(0xe156, fontFamily: 'MaterialIcons');
+  static const IconData deleteIcon = IconData(0xe8b6, fontFamily: 'MaterialIcons');
+
+
   ImagePicker picker = null;
   VideoPlayerController _galleryVideoPlayerController;
+
+  static const _chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
+  Random _rnd = Random();
+
+  String getRandomString(int length) => String.fromCharCodes(Iterable.generate(
+      length, (_) => _chars.codeUnitAt(_rnd.nextInt(_chars.length))));
 
   @override
   void initState() {
@@ -60,11 +73,29 @@ class _GalleryScreenState extends State<Gallery> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Expanded(
+            InkWell(
+              onTap:(){ _pickVideoFromGallery() ;},
                 child: Container(
               color: Colors.white,
               height: 100,
-            ))
+                  child: Column(
+                    children: <Widget>[
+                      Icon(deleteIcon,color: Colors.black,size: 50)
+                      
+                    ],
+                  ),
+            )),
+            InkWell(
+                onTap:(){ uploadVideoToFirebaseAndGoBack(_galleryVideo) ;},
+                child: Container(
+                  color: Colors.white,
+                  height: 100,
+                  child: Column(
+                    children: <Widget>[
+                      Icon(checkIcon,color: Colors.black,size: 50)
+                    ],
+                  ),
+                ))
           ],
         ),
       ])),
@@ -81,19 +112,24 @@ class _GalleryScreenState extends State<Gallery> {
         _galleryVideoPlayerController.play();
       });
 
-    uploadImageToFirebase(_galleryVideo);
+
   }
 
-  Future<void> uploadImageToFirebase(File galleryVideo) async {
+  Future<void> uploadVideoToFirebaseAndGoBack(File galleryVideo) async {
     Future<File> result = testCompressAndGetFile(galleryVideo,"");
    // print(galleryVideo.lengthSync());
     result.then((value) async => {
   //  print(value.length);
-        await _firebaseStorage.ref().child('videos/video2.mp4')
+      //TODO use user name in filename combination
+        await _firebaseStorage.ref().child('videos/' + getRandomString(7) + '.mp4')
         .putFile(value).onComplete
     });
 
-
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => FeedScreen(),
+      ),
+    );
   }
 
  Future<File> testCompressAndGetFile(File file, String targetPath) async {
